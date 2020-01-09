8cf3eeffc77e84a22b648799314b59a3a0b2b955
weekend79 <mortenviken@yahoo.com>
2020-01-09 22:39:33 +0000

    click to get the X and Oin the console

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,43 +4,43 @@ console.log('Your JS is linked up. Be the person you needed when you were little
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
-/*----- cached element references -----*/
+let win; 
 
+/*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'))
-//const messages = document.querySelector('h2');
+const messages = document.querySelector('h2');
 
 /*----- event listeners -----*/
-
-//document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('board').addEventListener('click', handleTurn);
 
 /*----- functions -----*/
-//function handleTurn(event) {
-//        let idx = squares.findIndex(function (square) {
-//            return square === event.target;
-//        });
-//        board[idx] = turn;
-//        console.log(board);
+function handleTurn(event) {
+    let idx = squares.findIndex(function (square) {
+        return square === event.target;
+    });
+            board[idx] = turn;
+            console.log(board);
 
-// ternaries
-//turn = turn === 'X' ? 'O' : 'X'
-// In an if statement it would look like this (below code perform the same as the code on the line above)
-// if (turn === 'X') {
-// turn = 'O'    
-// } else {
-// turn = 'X'
-// };
+    // ternaries
+    turn = turn === 'X' ? 'O' : 'X';
+    // In an if statement it would look like this (below code perform the same as the code on the line above)
+    // if (turn === 'X') {
+    // turn = 'O'    
+    // } else {
+    // turn = 'X'
+    // };
 
-//    };    
+};
 
 function render() {
 
-        board.forEach(function(mark, index) {
-            squares[index].textContent = mark;
+    board.forEach(function (val, idx) {
+        squares[idx].textContent = val;
 
-        });
+    });
 
-        //messages.textContent = `It's ${turn}'s turn!`;
-    };
+    messages.textContent = `It's ${turn}'s turn!`;
+};
 
 function init() {
     board = [
